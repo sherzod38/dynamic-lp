be4858f4efe0eed148b906a37e1d7b8ce92572f8
unknown <sherzod38>
2025-04-22 21:25:57 +0500

    4

--- components/hero-carousel.tsx
+++ components/hero-carousel.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, JSX, SVGProps } from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import { Button } from "../components/ui/button"
 import Image from "next/image"
 
 const carouselItems = [
